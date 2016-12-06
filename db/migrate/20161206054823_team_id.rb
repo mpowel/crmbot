50f889e0cf0d5be180c4c8e6c0a7828e5c6c6b1f
mpowel <mlp206@gmail.com>
2016-12-06 01:06:32 -0500

    z

--- db/migrate/20161206054823_team_id.rb
+++ db/migrate/20161206054823_team_id.rb
@@ -1,4 +1,17 @@
 class TeamId < ActiveRecord::Migration[5.0]
   def change
+    create_table :teams do |t|
+
+      # t.string    :access_token
+      # t.string    :team_name
+      t.int    :team_id
+      # t.string    :user_id
+      # t.text      :raw_json
+      # t.string    :incoming_webhook
+      # t.string    :incoming_channel
+      # t.string    :bot_token
+      # t.string    :bot_user_id
+      # t.boolean   :is_active, default: true
   end
+  has many :contacts  #, dependent: :destroy
 end
--- db/migrate/20161206054847_contact.rb
+++ db/migrate/20161206054847_contact.rb
@@ -1,4 +1,15 @@
 class Contact < ActiveRecord::Migration[5.0]
+
   def change
+    create_table :contacts do |t|
+
+      t.int       :team_id
+      t.string    :name
+      t.string    :gender
+      t.string    :title
+      t.string    :email
+      t.int       :phone
+      t.datetime  :timestamp
   end
+      belongs_to :team  #, dependent: :destroy
 end
