d8f5f4d1e660a1fef015604e2f4b7cd687362aed
mpowel <mlp206@gmail.com>
2016-12-08 13:42:43 -0500

    resyncing with daragh's files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,23 @@
-Team.destroy_all
+# Contact.create([{
+#     title: "Course Policies",
+#     url: "https://daraghbyrne.github.io/onlineprototypes2016/course/structure/policies/2016/01/07/course-policies/"
+#     week_id: 1
+#   },
+#   {
+#     title: "Course Slack",
+#     url: "https://onlineprototypes2016.slack.com/messages",
+#     week_id: 1
+#
+#   }])
+#
+#   Team.create([{
+#       title: "Course Policies",
+#       url: "https://daraghbyrne.github.io/onlineprototypes2016/course/structure/policies/2016/01/07/course-policies/"
+#       week_id: 1
+#     },
+#     {
+#       title: "Course Slack",
+#       url: "https://onlineprototypes2016.slack.com/messages",
+#       week_id: 1
+#
+#     }])
--- models/team.rb
+++ models/team.rb
@@ -1,20 +1,22 @@
 class Team < ActiveRecord::Base
   
-  #has_many :tasks, dependent: :destroy
+    #has_many :tasks, dependent: :destroy
   
-  # validates_presence_of :team_id
-  #
-  # has_many :contacts
+    validates_presence_of :team_id
+    validates_presence_of :user_id
+  
+    # has_many :office_hour_queues  #<--- this is different from sample
+    has_many :events
   
   
-  # def get_client
-  #
-  #   Slack.configure do |config|
-  #     config.token = self.bot_token
-  #   end
-  #
-  #   client = Slack::Web::Client.new
-  #
-  # end
+    def get_client
+    
+      Slack.configure do |config|
+        config.token = self.bot_token
+      end
+    
+      client = Slack::Web::Client.new
+    
+    end
   
-end
+  end
